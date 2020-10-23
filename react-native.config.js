57027b55ba850846112b3fe7a063c8969be7f91f
Bogdan <>
2020-10-23 15:57:26 +0300

    * new rn config file

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,20 @@
+let netinfo = require('@react-native-community/netinfo');
+let downloader = require('react-native-background-downloader');
+let svg = require('react-native-svg');
+let fBlob = require('rn-fetch-blob');
 module.exports = {
-  project: {
-    ios: {},
-    android: {}
+  dependencies: {
+    '@react-native-community/netinfo': {
+      root: netinfo
+    },
+    'react-native-background-downloader': {
+      root: downloader
+    },
+    'react-native-svg': {
+      root: svg
+    },
+    'rn-fetch-blob': {
+      root: fBlob
+    }
   }
 };
